21f8fac8eea1bf792b703cb27c2cdafa6cbadf07
zikai <lewisxzk@outlook.com>
2023-12-28 13:24:07 +0800

    add pc, under revised

--- calibration_cc.py
+++ calibration_cc.py
@@ -19,6 +19,11 @@ from functools import partial
 import re
 import numpy as np
 import string
+from sklearn.mixture import GaussianMixture
+from scipy.optimize import linear_sum_assignment
+import time
+from sklearn.decomposition import PCA
+
 
 # offline
 os.environ['HF_DATASETS_OFFLINE'] = '1'
@@ -40,7 +45,6 @@ try:
         device = "mps"
 except:
     pass
-
 
 
 def cleansed_response_for_acceptability(pred):
@@ -151,6 +155,7 @@ class Evaluator():
         with open(self.output_short_result_file_name, 'a') as f:
             f.write(str(index) + " " + str(result) + '\n')
 
+
 class Calibrator():
     def __init__(self, args, method, dataloader, evaluator):
         self.args = args
@@ -168,7 +173,11 @@ class Calibrator():
             return partial(self.batch_run, prob_mean=prob_mean) 
         elif self.method == "dc":
             prob_mean = self.estimate_bias_dc()
-            return partial(self.batch_run, prob_mean=prob_mean)      
+            return partial(self.batch_run, prob_mean=prob_mean)   
+        elif self.method == "pc":  # 添加Prototypical Calibration的逻辑
+            gmm = self.estimate_bias_pc()
+            assigned_labels = self.assign_clusters_to_labels(gmm)
+            return partial(self.batch_run_pc, gmm_model=gmm, assigned_labels=assigned_labels)
     
     def batch_run(self, batch_input, prob_mean):
         tokenized_inputs = self.tokenizer(batch_input['full_prompt'], padding='max_length', max_length=self.args.cutoff_len, return_tensors="pt")
@@ -227,6 +236,28 @@ class Calibrator():
         
         list_of_response = [self.prompter.get_response(res) for res in response]
         return batch_input['full_prompt'], response, list_of_response 
+    
+    
+    def batch_run_pc(self, batch_input, gmm_model, assigned_labels):
+        """
+        Inference (文章的Inference节)
+        """
+        tokenized_inputs = self.tokenizer(batch_input['full_prompt'], padding='max_length', max_length=self.args.cutoff_len, return_tensors="pt")
+        tokenized_inputs = tokenized_inputs.to(device)
+        with torch.no_grad():
+            # 获得模型的输出
+            outputs = self.model(input_ids=tokenized_inputs['input_ids'], attention_mask=tokenized_inputs['attention_mask'])
+            next_token_logits = outputs.logits[:, -1, :]
+            # 获得每个标签的概率
+            probabilities = F.softmax(next_token_logits, dim=-1).cpu().numpy()
+
+            # 使用GMM模型进行预测，找到最可能的聚类
+            cluster_assignments = gmm_model.predict(probabilities)
+
+            # 根据分配的聚类找到对应的标签
+            predicted_labels = [assigned_labels[assignment] for assignment in cluster_assignments]
+
+        return batch_input['full_prompt'], predicted_labels
     
     
     # percent: 用来估计bias的数据比例
@@ -328,7 +359,7 @@ class Calibrator():
         for i, batch_input in enumerate(tqdm(dataloader, desc="Evaluating", total=batches_to_process)):
             if i >= batches_to_process:
                 break  
-            tokenized_inputs = self.tokenizer(self.replace_input_with_NA_in_batch(batch_input['full_prompt'], content="N/A"), padding='max_length', max_length=self.args.cutoff_len, return_tensors="pt")
+            tokenized_inputs = self.tokenizer(batch_input['full_prompt'], padding='max_length', max_length=self.args.cutoff_len, return_tensors="pt")
             tokenized_inputs = tokenized_inputs.to(device)
             generation_config = GenerationConfig(
                 max_new_tokens=10,
@@ -350,6 +381,134 @@ class Calibrator():
         return torch.cat(logits_list, dim=0).mean(dim=0)
 
 
+    def estimate_bias_pc(self, percent=0.1):
+        """
+        Prototypical Cluster Estimation (文章的第32节)
+        """
+        # 收集所有样本的概率分布
+        all_probabilities = []
+        total_batches = len(self.dataloader)
+        batches_to_process = int(percent * total_batches)
+        for i, batch_input in enumerate(tqdm(self.dataloader, desc="Evaluating", total=batches_to_process)):
+            if i >= batches_to_process:
+                break
+            tokenized_inputs = self.tokenizer(batch_input['full_prompt'], padding='max_length', max_length=self.args.cutoff_len, return_tensors="pt")
+            tokenized_inputs = tokenized_inputs.to(device)
+            with torch.no_grad():
+                outputs = self.model(input_ids=tokenized_inputs['input_ids'], attention_mask=tokenized_inputs['attention_mask'])
+                next_token_logits = outputs.logits[:, -1, :]
+                probabilities = F.softmax(next_token_logits, dim=-1).cpu().numpy()
+                all_probabilities.append(probabilities)
+
+        # 将所有概率堆叠成一个大数组
+        all_probabilities = np.vstack(all_probabilities)
+
+        # GMM的参数
+        if args.dataset == "cola":
+            self.n_clusters = 2  # 分类任务的类别数
+        else:
+            raise ValueError("请指定分类类别数量。当前数据集不支持或未定义处理逻辑。")
+        self.random_state = 42  # 随机种子
+        self.n_init = 10  # 初始化次数
+        
+        # 设置降维后的维度
+        n_components_pca = 50  # 可以根据需要调整
+
+        # 进行 PCA 降维
+        pca = PCA(n_components=n_components_pca)
+        all_probabilities = pca.fit_transform(all_probabilities)
+
+        # 使用GMM进行原型聚类估计
+        # 开始计时
+        print("GMM start...")
+        start_time = time.time()
+        gmm = GaussianMixture(n_components=self.n_clusters, covariance_type='full', random_state=self.random_state, n_init=self.n_init)
+        gmm.fit(all_probabilities)
+        end_time = time.time()
+        time_taken = end_time - start_time
+        print("GMM takes {} seconds.".format(time_taken))
+        # 返回GMM模型
+        return gmm
+    
+    def assign_clusters_to_labels(self, gmm_model):
+        """
+        Cluster-Label Assignment (文章的Cluster-Label Assignment节)
+        """
+        # 获取GMM模型的聚类中心
+        cluster_centers = gmm_model.means_
+        num_clusters = cluster_centers.shape[0]
+
+        # 初始化距离矩阵
+        if args.dataset == "cola":
+            distance_matrix = np.zeros((num_clusters, 2))
+            # 获取'acceptable' 和 'unacceptable'的嵌入
+            acceptable_idx = self.tokenizer.convert_tokens_to_ids('accept')
+            unacceptable_idx = self.tokenizer.convert_tokens_to_ids('un')
+            acceptable_embedding = self.tokenizer_embeddings[acceptable_idx] 
+            unacceptable_embedding = self.tokenizer_embeddings[unacceptable_idx]
+            # 计算距离
+            for i, center in enumerate(cluster_centers):
+                distance_matrix[i, 0] = np.linalg.norm(center - acceptable_embedding)
+                distance_matrix[i, 1] = np.linalg.norm(center - unacceptable_embedding)
+        elif args.dataset == "quail":
+            distance_matrix = np.zeros((num_clusters, 4))
+            # 假设四个标签为 'A', 'B', 'C', 'D'，并获取它们的嵌入
+            label_indices = {'A': self.tokenizer.convert_tokens_to_ids('A'),
+                            'B': self.tokenizer.convert_tokens_to_ids('B'),
+                            'C': self.tokenizer.convert_tokens_to_ids('C'),
+                            'D': self.tokenizer.convert_tokens_to_ids('D')}
+            label_embeddings = {label: self.tokenizer_embeddings[idx] for label, idx in label_indices.items()}
+            # 计算距离
+            for i, center in enumerate(cluster_centers):
+                for j, (label, embedding) in enumerate(label_embeddings.items()):
+                    distance_matrix[i, j] = np.linalg.norm(center - embedding)
+        else:
+            raise ValueError("当前数据集不支持或未定义处理逻辑。")
+
+        # 使用匈牙利算法找到最佳匹配
+        cluster_indices, label_indices = linear_sum_assignment(distance_matrix)
+
+        # 将聚类索引映射到实际标签
+        if args.dataset == "cola":
+            assigned_labels = {cluster_idx: 'accept' if label_idx == 0 else 'un' for cluster_idx, label_idx in zip(cluster_indices, label_indices)}
+        elif args.dataset == "quail":
+            label_list = list(label_indices.keys())
+            assigned_labels = {cluster_idx: label_list[label_idx] for cluster_idx, label_idx in zip(cluster_indices, label_indices)}
+
+        return assigned_labels
+
+    def select_best_estimation(self, estimations):
+        """
+        Estimation Selection Based on Cluster-Label Assignment (文章的第33节)
+        """
+        best_score = -np.inf
+        best_estimation = None
+
+        for estimation in estimations:
+            # 计算每个估计的聚类中心与标签的分配
+            assigned_labels = self.assign_clusters_to_labels(estimation)
+
+            # 计算CLA分数
+            cla_score = self.compute_cla_score(estimation, assigned_labels)
+
+            # 选择CLA分数最高的估计
+            if cla_score > best_score:
+                best_score = cla_score
+                best_estimation = estimation
+
+        return best_estimation
+
+    def compute_cla_score(self, gmm_model, assigned_labels):
+        """
+        计算给定估计的CLA分数
+        """
+        cluster_centers = gmm_model.means_
+        score = 0
+        for cluster_idx, label_idx in assigned_labels.items():
+            center = cluster_centers[cluster_idx]
+            label_embedding = self.tokenizer_embeddings[label_idx]
+            score += -np.linalg.norm(center - label_embedding)  # 使用负欧几里得距离作为得分
+        return score
 
 
 def batch_eva_write_to_excel(num_communication_rounds, args, write_to_excel=True, metrics='accurcay', positive_label=None, use_trained_model=True):
@@ -389,7 +548,7 @@ def batch_eva_write_to_excel(num_communication_rounds, args, write_to_excel=True
         labels = []
 
         # 做个矫正
-        cali_method = "dc"  # 或 "dc"
+        cali_method = "pc"  # 或 "dc", "pc"
         calibrator = Calibrator(args, cali_method, dataloader, evaluator)
         cali_batch_run = calibrator.run()
         
